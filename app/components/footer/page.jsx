74c9d9437da70d7f6b4fb3f1421443e579408eed
imVey <marwanbella91@hotmail.fr>
2024-06-28 18:14:43 +0200

    Feat (footer) : add element in footer

--- app/components/footer/page.jsx
+++ app/components/footer/page.jsx
@@ -1,10 +1,29 @@
 // components/Footer.js
 const Footer = () => {
-    return (
-        <footer>
-            <p>&copy; {new Date().getFullYear()} My Next.js App. All rights reserved.</p>
-        </footer>
-    );
+  return (
+    <footer>
+      <p>
+        &copy; {new Date().getFullYear()} My Next.js App. All rights reserved.
+      </p>
+      <div className="flex gap-52 mt-10 mb-10 ml-52">
+        <section className="flex flex-col items-start">
+          <a href="/terms-of-service">Terms of Service </a>
+          <a href="/privacy-policy">Privacy Policy </a>
+          <a href="/cookie-policy">Cookie Policy </a>
+        </section>
+        <section className="flex flex-col items-start">
+          <a href="/sitemap.xml">Sitemap </a>
+          <a href="/accessibility-statement">Accessibility Statement </a>
+          <a href="/contact">Contact </a>
+        </section>
+        <section className="flex flex-col items-start">
+          <a href="https://github.com/imVey">GitHub </a>
+          <a href="https://www.linkedin.com/in/bellamarwan/">LinkedIn </a>
+          <a href="https://twitter.com/">Twitter </a>
+        </section>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
